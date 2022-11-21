9ac4d00879f5751b07b96dd388fda76ffab0f8be
martinideniam <martinideniam@gmail.com>
2022-11-21 20:40:40 +0000

    project completed

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 // https://api.adviceslip.com/advice
 // will generate random advice 
-console.log("hello")
+
+let btnEl = document.querySelector(".advice-btn")
+btnEl.addEventListener("click", () => {
+    displayAdvice()
+})
+
+
+async function fetchAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json()
+    return data
+}
+
+async function displayAdvice() {
+    let adviceTitleEl = document.querySelector('.advice-title')
+    let adviceTextEl = document.querySelector('.advice-text')
+    let data = await fetchAdvice()
+    adviceTitleEl.innerHTML = `ADVICE #${data['slip']['id']}`
+    adviceTextEl.innerHTML = `${data['slip']['advice']}`
+    console.log(data)
+}
