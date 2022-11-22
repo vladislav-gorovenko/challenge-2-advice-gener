c8feb941305c71bd16bd1aeb034fdf9fccf00538
martinideniam <martinideniam@gmail.com>
2022-11-22 08:39:23 +0000

    added loading/fetching animation for button / reduced the height of advice-box

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ btnEl.addEventListener("click", () => {
 
 
 async function fetchAdvice() {
+    if (!btnEl.classList.contains("loading")) {
+        btnEl.classList.add("loading")
+    }
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json()
     return data
@@ -17,6 +20,10 @@ async function displayAdvice() {
     let adviceTitleEl = document.querySelector('.advice-title')
     let adviceTextEl = document.querySelector('.advice-text')
     let data = await fetchAdvice()
+    if (btnEl.classList.contains("loading")) {
+        btnEl.classList.remove("loading")
+    }
+    btnEl.classList.remove("loading")
     adviceTitleEl.innerHTML = `ADVICE #${data['slip']['id']}`
     adviceTextEl.innerHTML = `${data['slip']['advice']}`
     console.log(data)
